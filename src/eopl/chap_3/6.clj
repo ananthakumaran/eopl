cef48ed18ee4ad63500b8697d748d1156bc316f3
Anantha Kumaran <ananthakumaran@gmail.com>
2012-03-26 19:48:43 +0530

    cleanup

--- src/eopl/chap_3/6.clj
+++ src/eopl/chap_3/6.clj
@@ -1,16 +1,13 @@
-;; Extend the language by adding operators for addition,
-;; multiplication and integer quotient
+;; Extend the language by adding a new operator minus that takes one
+;; argument n and returns -n. For example, the value
+;; minus(-(minus(5),9)) should be 14
 
 (ns eopl.chap-3.6
   (:use eopl.core.let-lang)
   (:use clojure.test))
 
 (deftest minus-test
-  (is (= (result "+(1,2)")
-         3))
-  (is (= (result "*(2,2)")
-         4))
-  (is (= (result "/(10,5)")
-         2)))
+  (is (= (result "minus(-(minus(5),9))")
+         14)))
 
 (run-tests)
---
+++ src/eopl/chap_3/7.clj
@@ -0,0 +1,16 @@
+;; Extend the language by adding operators for addition,
+;; multiplication and integer quotient
+
+(ns eopl.chap-3.7
+  (:use eopl.core.let-lang)
+  (:use clojure.test))
+
+(deftest minus-test
+  (is (= (result "+(1,2)")
+         3))
+  (is (= (result "*(2,2)")
+         4))
+  (is (= (result "/(10,5)")
+         2)))
+
+(run-tests)
