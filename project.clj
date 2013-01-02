99b47b8493403a5c59c8dbf1454ba370f6e35cd9
Anantha Kumaran <ananthakumaran@gmail.com>
2013-01-02 12:43:14 +0530

    upgrade to lein 2 & clojure 1.4

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject eopl "1.0.0-SNAPSHOT"
   :description "epol exercises"
-  :dependencies [[org.clojure/clojure "1.2.1"]
-                 [fnparse "2.2.7"]]
-  :plugins [[lein-swank "1.4.4"]]
-  :test-path "src")
+  :dependencies [[org.clojure/clojure "1.4.0"]
+                 [org.clojars.jpoplett/fnparse "2.2.8"]]
+  :test-paths ["src"])
+
