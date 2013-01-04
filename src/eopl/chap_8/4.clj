876d6c2e44865e2c89a05a683d99a289c71f1487
Anantha Kumaran <ananthakumaran@gmail.com>
2013-01-04 18:13:49 +0530

    8.4

---
+++ src/eopl/chap_8/4.clj
@@ -0,0 +1,6 @@
+;; Change the expression language to include multiple let
+;; declarations, multiargument procedures, and multiple letrec
+;; declarations, as in exercise 7.24.
+
+;; see eopl.core.simple-module-lang
+
